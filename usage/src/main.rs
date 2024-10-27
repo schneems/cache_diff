01c4dab65ddc365362eb7a2c5640a2b73d300faf
Schneems <richard.schneeman+foo@gmail.com>
2024-10-26 20:25:44 -0500

    Ensure trait is used and fix warnings

--- usage/src/main.rs
+++ usage/src/main.rs
@@ -3,6 +3,7 @@ use cache_diff::CacheDiff;
 #[derive(CacheDiff)]
 struct Hello {}
 fn main() {
+    let _ = Hello {};
     println!("Hello, world!");
 }
 
@@ -10,18 +11,20 @@ fn main() {
 mod tests {
     use super::*;
 
+    fn is_diff<T: cache_diff::CacheDiff>(_in: &T) {}
+
     #[test]
     fn test_cache_diff() {
         #[derive(CacheDiff)]
         struct Person {
-            name: String,
+            _name: String,
         }
-
-        let diff = Person {
-            name: "richard".to_string(),
-        }
-        .diff(&Person {
-            name: "rich".to_string(),
+        let richard = Person {
+            _name: "richard".to_string(),
+        };
+        is_diff(&richard);
+        let diff = richard.diff(&Person {
+            _name: "rich".to_string(),
         });
 
         assert_eq!(diff.len(), 1);
